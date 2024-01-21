8ffd195cbc81da84f665affd7d26ef14f9323563
Jyotiraditya Singh <96012244+singhjyotiraditya@users.noreply.github.com>
2024-01-21 22:04:56 +0530

    Time: 11 ms (89.42%), Space: 19.3 MB (20.63%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,14 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n=nums.size();
-        if (n==1){
-           return nums[0];
+    int singleNumber(std::vector<int>& nums) {
+        int n = nums.size();
+        int xorr =0;
+        for (int i=0; i<n; i++){
+            xorr = xorr^nums[i];
         }
-        for (int i=1; i<n; i+=2){
-           if (nums[i]!=nums[i-1]){
-               return nums[i-1];
-           }
-        }
-        return nums[n-1];
+        return xorr;
     }
 };
